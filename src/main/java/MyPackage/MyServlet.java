c0e53e0ca87ce50204a8c9582141a93fec2c70f7
Anupam <anupam200024@gmail.com>
2024-01-29 22:36:43 +0530

    files added

---
+++ src/main/java/MyPackage/MyServlet.java
@@ -0,0 +1,117 @@
+package MyPackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String apiKey = "36bab91beec3e2fceffce7a85533b111";
+		String city = request.getParameter("city");
+		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+		// API Integration
+		URL url = new URL(apiUrl);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		// Read Data Stream from Network
+		InputStream inputStream = connection.getInputStream();
+		InputStreamReader reader = new InputStreamReader(inputStream);
+
+		// want to store in string
+		StringBuilder responseContent = new StringBuilder();
+
+		// Input from the reader, will create scanner object
+		Scanner scanner = new Scanner(reader);
+
+		while (scanner.hasNext()) {
+			responseContent.append(scanner.nextLine());
+		}
+		scanner.close();
+
+		// TypeCasting = Parsing the data into JSON
+		Gson gson = new Gson();
+		JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+//		System.out.println(jsonObject);
+
+		// Date and Time
+		long dateTimeStamp = jsonObject.get("dt").getAsLong() * 1000;
+		SimpleDateFormat sdf = new java.text.SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy"); 
+	    sdf.setTimeZone(java.util.TimeZone.getTimeZone("Asia/Kolkata")); 
+	    String date = sdf.format(dateTimeStamp);
+
+		// Temperature
+		double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+		int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+
+		// Humidity
+		int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+
+		// Wind Speed
+		double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+
+		// Weather Condition
+		String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main")
+				.getAsString();
+
+		// Set the data as request attribute (for sending to the jsp page)
+		request.setAttribute("date", date);
+		request.setAttribute("city", city);
+		request.setAttribute("temperature", temperatureCelsius);
+		request.setAttribute("weatherCondition", weatherCondition);
+		request.setAttribute("humidity", humidity);
+		request.setAttribute("windSpeed", windSpeed);
+		request.setAttribute("weatherData", responseContent.toString());
+
+		connection.disconnect();
+
+		// Forward the request to the weather.jsp page for rendering
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+	}
+
+}
